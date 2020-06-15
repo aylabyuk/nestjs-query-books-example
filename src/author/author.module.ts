094a11f42e21e7fbe7a530d3f370bb587f215491
Oriel Vinci Absin <oriel.absin@gmail.com>
2020-06-15 12:55:24 +0800

    use crudresolver

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -3,16 +3,19 @@ import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql'
 import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm'
 import { AuthorEntity } from './author.entity'
 import { Author } from './author.dto'
+import { AuthorResolver } from './author.resolver'
 
 @Module({
+  providers: [AuthorResolver],
   imports: [
-    NestjsQueryGraphQLModule.forFeature({
-      imports: [NestjsQueryTypeOrmModule.forFeature([AuthorEntity])],
-      resolvers: [{
-        DTOClass: Author,
-        EntityClass: AuthorEntity
-      }]
-    })
+    NestjsQueryTypeOrmModule.forFeature([AuthorEntity])
+    // NestjsQueryGraphQLModule.forFeature({
+    //   imports: [NestjsQueryTypeOrmModule.forFeature([AuthorEntity])],
+    //   resolvers: [{
+    //     DTOClass: Author,
+    //     EntityClass: AuthorEntity
+    //   }]
+    // })
   ]
 })
 export class AuthorModule {}
---
+++ src/author/author.resolver.ts
@@ -0,0 +1,44 @@
+import { QueryService, InjectQueryService } from '@nestjs-query/core'
+import { CRUDResolver } from '@nestjs-query/query-graphql'
+import { Resolver, Query, Args, Int, ResolveField } from '@nestjs/graphql'
+import { Author } from './author.dto'
+import { AuthorEntity } from './author.entity'
+
+@Resolver(() => Author)
+export class AuthorResolver extends CRUDResolver(Author, {
+  read: { one: { disabled: true } },
+}) {
+  constructor(
+    @InjectQueryService(AuthorEntity)
+    readonly service: QueryService<AuthorEntity>,
+  ) {
+    super(service)
+  }
+
+
+  // returning plain JSON
+  // I want to override the default (read one) resolver, and I dont want to invoke any database request. 
+  @Query(() => Author)
+  async author(@Args('id', { type: () => Int }) id: number) {
+    console.log('custom resolver running')
+    return {
+      "id": 7,
+      "email": "Hortense84@hotmail.com",
+      "firstName": "Adonis",
+      "lastName": "Boyle",
+      "books": {
+        "edges": [
+          {
+            "node": {
+              "id": 6,
+              "isPublished": false,
+              "title": "a exercitationem atque",
+              "summary": "Corporis magni in quod consequatur dolor aut atque veritatis. Dolor similique quisquam facilis quae minima ducimus. Mollitia eos nam ratione aliquid tempora dolore. Eveniet possimus ex. Omnis nam placeat.",
+              "publishedDate": "2020-02-13"
+            }
+          },
+        ]
+      }
+    }
+  }
+}
